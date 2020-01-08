e21f080aabd686729c0e16f9ed7d1cd6b785e939
Shihao Cao <shihaocao@gmail.com>
2020-01-08 00:17:05 -0500

    1ms lag time

--- src/FCCode/BMPMonitor.cpp
+++ src/FCCode/BMPMonitor.cpp
@@ -25,7 +25,7 @@ BMPMonitor::BMPMonitor(StateFieldRegistry &registry,
                         Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
                         Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                         Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                        Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+                        Adafruit_BMP280::STANDBY_MS_1); /* Standby time. */
 
     }
 
