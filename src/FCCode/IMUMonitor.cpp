931359849b7a4917a00151764c97176ca1a73027
Shihao Cao <shihaocao@gmail.com>
2020-01-12 17:07:09 -0500

    imu monitor changes

--- src/FCCode/IMUMonitor.cpp
+++ src/FCCode/IMUMonitor.cpp
@@ -53,11 +53,40 @@ void IMUMonitor::execute(){
 
     //IF THIS IS TOO SLOW, DELETE READ OPERATIONS OF ^VECTORS WE DON'T NEED
 
+    //IF BELOW TOO SLOW, STOP USING SENSOR EVENTS YIKES
     //dump temporary containers into statefields
-    linear_acc_vec_f.set(f_vector_t{linear_acc_vec.acceleration.x, linear_acc_vec.acceleration.y, linear_acc_vec.acceleration.z});
-    // acc_vec_f.set(acc_vec);
-    // grav_vec_f.set(grav_vec);
-    // euler_vec_f.set(euler_vec);
-    // gyr_vec_f.set(gyr_vec);
-    // mag_vec_f.set(mag_vec);
+    linear_acc_vec_f.set(f_vector_t{
+        linear_acc_vec.acceleration.x, 
+        linear_acc_vec.acceleration.y, 
+        linear_acc_vec.acceleration.z});
+
+    acc_vec_f.set(f_vector_t{
+        acc_vec.acceleration.x, 
+        acc_vec.acceleration.y, 
+        acc_vec.acceleration.z
+    });
+
+    grav_vec_f.set(f_vector_t{
+        grav_vec.acceleration.x,
+        grav_vec.acceleration.y,
+        grav_vec.acceleration.z
+    });
+
+    euler_vec_f.set(f_vector_t{
+        euler_vec.orientation.x,
+        euler_vec.orientation.y,
+        euler_vec.orientation.z,
+    });
+    
+    gyr_vec_f.set(f_vector_t{
+        gyr_vec.gyro.x,
+        gyr_vec.gyro.y,
+        gyr_vec.gyro.z,
+    });
+
+    mag_vec_f.set(f_vector_t{
+        mag_vec.magnetic.x,
+        mag_vec.magnetic.y,
+        mag_vec.magnetic.z
+    });
 }
