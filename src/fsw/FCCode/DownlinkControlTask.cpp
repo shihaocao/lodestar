5939ab597e3ccd2888721e3c93aae15f23c1d26c
Shihao Cao <shehowcow@gmail.com>
2020-05-13 22:54:36 -0400

    smooth out downlinking

--- src/fsw/FCCode/DownlinkControlTask.cpp
+++ src/fsw/FCCode/DownlinkControlTask.cpp
@@ -9,74 +9,75 @@ DownlinkControlTask::DownlinkControlTask(StateFieldRegistry &registry,
         acc_fp = find_internal_field<f_vector_t>("imu.acc_vec", __FILE__, __LINE__);
         linear_acc_fp = find_internal_field<f_vector_t>("imu.linear_acc_vec", __FILE__, __LINE__);
         gyr_fp = find_internal_field<f_vector_t>("imu.gyr_vec", __FILE__, __LINE__);
+        quat_fp = find_internal_field<d_quat_t>("imu.quat", __FILE__, __LINE__);
 
+        #if defined(HARDLINE)
         Serial.begin(9600);
+        #endif
 
-        // #if defined(SERIAL_DEBUG_COMPACT) || defined(SERIAL_DEBUG) 
-        // Serial.begin(9600);
-        // #endif
-
-        #if defined(FLIGHT) || defined(SERIAL_DEBUG_915) 
+        #if defined(AIR)
         Serial1.begin(57600);
         #endif
     }
 
 void DownlinkControlTask::execute(){
 
-    f_vector_t linear_acc_read;
-    f_vector_t acc_read;
-    f_vector_t euler_read;
-    f_vector_t gyr_read;
-
-    linear_acc_read = linear_acc_fp->get();
-    acc_read = acc_fp->get();
-    euler_read = euler_fp->get();
-    gyr_read = gyr_fp->get();
-
     #ifdef DL_OFF
     return;
     #endif
+
+    #ifndef DL_OFF
+    f_vector_t linear_acc_read = linear_acc_fp->get();
+    f_vector_t acc_read = acc_fp->get();
+    f_vector_t euler_read = euler_fp->get();
+    f_vector_t gyr_read = gyr_fp->get();
+    d_quat_t quat_read = quat_fp->get();
+    #endif
+
 
     // unsigned int temp = PAN::control_cycle_time;
     #ifdef SPEED_TEST
     Serial.printf("CN: %u, CCT: %u\n", control_cycle_count, PAN::control_cycle_time_ms);
     #endif
 
-    #ifdef FLIGHT
-    Serial1.printf("%u,", control_cycle_count);
-    Serial1.printf("%f,", altitude_fp->get());
-    Serial1.printf("%f,%f,%f,", linear_acc_read[0], linear_acc_read[1], linear_acc_read[2]);
-    Serial1.printf("%f,%f,%f,", acc_read[0], acc_read[1], acc_read[2]);
-    Serial1.printf("%f,%f,%f,", euler_read[0], euler_read[1], euler_read[2]);
-    Serial1.printf("%f,%f,%f", gyr_read[0], gyr_read[1], gyr_read[2]);
+    #if defined(AIR) && defined(COMPACT)
+    airline_element(control_cycle_count);
+    airline_element(altitude_fp->get());
+    airline_compact(linear_acc_read);
+    airline_compact(acc_read);
+    airline_compact(euler_read);
+    airline_compact(gyr_read);
+    airline_compact(quat_read);
     Serial1.print("\n");
     #endif
 
-    #ifdef SERIAL_DEBUG
+    #if defined(HARDLINE) && defined(COMPACT)
+    hardline_element(control_cycle_count);
+    hardline_element(altitude_fp->get());
+    hardline_compact(linear_acc_read);
+    hardline_compact(acc_read);
+    hardline_compact(euler_read);
+    hardline_compact(gyr_read);
+    hardline_compact(quat_read);
+    Serial.print("\n");
+    #endif
+
+    #if defined(HARDLINE) && defined(FULL_DL)
     Serial.printf("Control Cycle Num: %u\n", control_cycle_count);
     Serial.printf("Altitude (m): %f\n", altitude_fp->get());
     Serial.printf("Linear Acc: %f, %f, %f\n", linear_acc_read[0], linear_acc_read[1], linear_acc_read[2]);
     Serial.printf("Acc: %f, %f, %f\n", acc_read[0], acc_read[1], acc_read[2]);
     Serial.printf("Euler: %f, %f, %f\n", euler_read[0], euler_read[1], euler_read[2]);
     Serial.printf("Gyr: %f, %f, %f\n", gyr_read[0], gyr_read[1], gyr_read[2]);
+    Serial.print("\n");
     #endif
 
-    #ifdef SERIAL_DEBUG_915
+    #if defined(AIR) && defined(FULL_DL)
     Serial1.printf("Control Cycle Num: %u\n", control_cycle_count);
     Serial1.printf("Altitude (m): %f\n", altitude_fp->get());
     Serial1.printf("Linear Acc: %f, %f, %f\n", linear_acc_read[0], linear_acc_read[1], linear_acc_read[2]);
     Serial1.printf("Acc: %f, %f, %f\n", acc_read[0], acc_read[1], acc_read[2]);
     Serial1.printf("Euler: %f, %f, %f\n", euler_read[0], euler_read[1], euler_read[2]);
     Serial1.printf("Gyr: %f, %f, %f\n", gyr_read[0], gyr_read[1], gyr_read[2]);
-    #endif
-
-    #ifdef SERIAL_DEBUG_COMPACT
-    Serial.printf("%u,", control_cycle_count);
-    Serial.printf("%f,", altitude_fp->get());
-    Serial.printf("%f,%f,%f,", linear_acc_read[0], linear_acc_read[1], linear_acc_read[2]);
-    Serial.printf("%f,%f,%f,", acc_read[0], acc_read[1], acc_read[2]);
-    Serial.printf("%f,%f,%f,", euler_read[0], euler_read[1], euler_read[2]);
-    Serial.printf("%f,%f,%f", gyr_read[0], gyr_read[1], gyr_read[2]);
-    Serial.print("\n");
     #endif
 }
