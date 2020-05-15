029e3a7bac3a4d1460f020d86bd56247052c075b
Shihao Cao <shehowcow@gmail.com>
2020-05-14 23:24:42 -0400

    halfway through telem packet

--- web/flask/app.py
+++ web/flask/app.py
@@ -4,6 +4,14 @@ import threading
 import time
 import serial
 
+import json 
+
+with open('telem.json') as f:
+  telem_index = json.load(f)
+reverse_telem_index = {v:k for (k,v) in telem_index.items()}
+# dummy data to be over written
+telem_data = {k:v for (k,v) in telem_index.items()}
+
 POOL_TIME = 0.001
 altitude = 0
 incr = 0
@@ -11,7 +19,10 @@ ccno = 0
 dataLock = threading.Lock()
 threadHandler = threading.Thread()
 
-telem = serial.Serial('/dev/ttyACM0')
+
+# telem = serial.Serial('/dev/ttyACM0')
+telem = serial.Serial('/dev/ttyUSB0', 57600)
+telem.reset_input_buffer()
 
 def str_to_val(field):
     '''
@@ -30,10 +41,21 @@ def str_to_val(field):
     else:
         return int(field)
 
+def parse_token(ret):
+    if ',' in ret:
+        # ret is a list
+        list_of_strings = ret.split(',')
+        list_of_strings = [x for x in list_of_strings if x is not '']
+        list_of_vals = [str_to_val(x) for x in list_of_strings]
+        return list_of_vals
+    else:
+        return str_to_val(ret)
+
+
 def listgen(input_string):
-    ret = input_string.split(",")
+    ret = input_string.split(";")
     ret = [x for x in ret if x != '']
-    ret = [str_to_val(x) for x in ret]
+    ret = [parse_token(x) for x in ret]
     return ret
 
 
@@ -46,14 +68,26 @@ def doStuff():
     global threadHandler
     global incr
     global ccno
+    global telem_index, telem_data
+
     with dataLock:
     # Do your stuff with commonDataStruct Here
-        telem_data = listgen(telem.readline().strip().decode("utf-8"))
-        # print(telem_data)
-        ccno = telem_data[0]
-        # print(ccno)
-        altitude = telem_data[1]
-        incr += 1
+        raw_bytes = telem.readline().strip().decode("utf-8")
+        # print(raw_bytes)
+        tokens = listgen(raw_bytes)
+        if len(tokens) != len(telem_index):
+            print(f"INVALID NUMBER TOKENS, got: {len(tokens)}, expected: {len(telem_index)}")
+            
+        else:
+            for i in range(len(tokens)):
+                telem_data[reverse_telem_index[i]] = tokens[i]
+
+            # print(telem_data)
+            ccno = tokens[0]
+            # print(ccno)
+            altitude = tokens[1]
+            incr += 1
+
     # Set the next thread to happen
     threadHandler = threading.Timer(POOL_TIME, doStuff, ())
     threadHandler.start()   
@@ -94,3 +128,9 @@ def get_altitudes():
     global dataLock, altitude, ccno
     with dataLock:
         return jsonify(cc=ccno,alt = altitude)
+
+@app.route('/telem_packet')
+def get_telem_packet():
+    global dataLock, telem_data
+    with dataLock:
+        return jsonify(telem_data)
