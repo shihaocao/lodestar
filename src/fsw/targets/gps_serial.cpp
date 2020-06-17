0ab6d5d66e3c681fa184e3d8fa86dbb769eab8a3
Shihao Cao <shehowcow@gmail.com>
2020-06-17 00:16:50 -0400

    changed to nmea float decimal coords

--- src/fsw/targets/gps_serial.cpp
+++ src/fsw/targets/gps_serial.cpp
@@ -189,10 +189,10 @@ void loop() // run over and over again
     Serial.println(GPS.secondsSinceDate(), 3);
     if (GPS.fix) {
       Serial.print("Location: ");
-      Serial.print(GPS.latitude_fixed, 4);
+      Serial.print(GPS.latitudeDegrees, 4);
       Serial.print(GPS.lat);
       Serial.print(", ");
-      Serial.print(GPS.longitude_fixed, 4);
+      Serial.print(GPS.longitudeDegrees, 4);
       Serial.println(GPS.lon);
       Serial.print("Speed (knots): ");
       Serial.println(GPS.speed);
